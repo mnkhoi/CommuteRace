8b26c643ccae1a9f976a59ab5c384025aa9056d8
Khoi Nguyen <bob.k.nguyen@gmail.com>
2025-10-25 00:19:24 -0400

    [ADD] generating spring boot backend / react native frontend

---
+++ backend/src/main/java/com/mnkhoi/CommuteRace/CommuteRaceApplication.java
@@ -0,0 +1,13 @@
+package com.mnkhoi.CommuteRace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuteRaceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mnkhoi/CommuteRace/CommuteRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnkhoi.CommuteRace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuteRaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
